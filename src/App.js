3af1fcd6e294ea2bb20a78238a744e92067a56ce
Anthony <anthony.tomson1@gmail.com>
2018-10-25 21:42:02 +0200

    V1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import RefreshIcon from '@material-ui/icons/Refresh';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      first: ['Tromblon', 'Piston', 'Coulis', 'Pompier', 'Pangolin', 'Bourru'],
-      second: ['avacheur', 'tripoteur', 'rouilleur', 'sculpteur', 'sussoteur', 'titilleur'],
-      third: ['de slip', 'du Nord', 'du syphon', 'de fesse droite', 'ouzbek']
+      first: ['Tromblon', 'Piston', 'Coulis', 'Pompier', 'Pangolin', 'Bourru', 'Steak', 'Rognon', 'Marcassin', 'SDF'],
+      second: ['avacheur', 'tripoteur', 'rouilleur', 'sculpteur', 'sussoteur', 'titilleur', 'bricoleur', 'communiste', 'gratteur', 'voyeur', 'ivre'],
+      third: ['de slip', 'du Nord', 'du syphon', 'de fesse droite', `d'insanités`, 'de tournevis', 'du flap', 'de la haute bourgeoisie'],
+      firstR: 0,
+      secondR: 0,
+      thirdR: 0
     };
   }
-  render() {
-    const first = Math.floor((Math.random() * this.state.first.length));
-    const second = Math.floor((Math.random() * this.state.second.length));
-    const third = Math.floor((Math.random() * this.state.third.length));
 
+  componentDidMount() {
+    this.setState({
+      firstR: this.randomizer(this.state.first.length),
+      secondR: this.randomizer(this.state.second.length),
+      thirdR: this.randomizer(this.state.third.length)
+    });
+  }
+
+  reload = () => {
+    this.setState({
+      firstR: this.randomizer(this.state.first.length),
+      secondR: this.randomizer(this.state.second.length),
+      thirdR: this.randomizer(this.state.third.length)
+    });
+  }
+
+  randomizer(length) {
+    return Math.floor((Math.random() * length));
+  }
+
+  render() {
+    const { first, second, third, firstR, secondR, thirdR } = this.state;
     return (
       <div className="App">
-        <p>{this.state.first[first]} {this.state.second[second]} {this.state.third[third]}</p>
+        <header className="App-header">
+          <p>{first[firstR]} {second[secondR]} {third[thirdR]}</p>
+          <Button
+            onClick={this.reload}
+            variant="contained"
+            color="primary"
+          >
+            Reload
+            <RefreshIcon />
+          </Button>
+        </header>
       </div>
+
     );
   }
 }
