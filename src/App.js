50758577ff24687224464ba00c20a702b4a8e069
Anthony <anthony.tomson1@gmail.com>
2018-10-25 20:51:09 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      first: ['Tromblon', 'Piston', 'Coulis', 'Pompier', 'Pangolin', 'Bourru'],
+      second: ['avacheur', 'tripoteur', 'rouilleur', 'sculpteur', 'sussoteur', 'titilleur'],
+      third: ['de slip', 'du Nord', 'du syphon', 'de fesse droite', 'ouzbek']
+    };
+  }
   render() {
+    const first = Math.floor((Math.random() * this.state.first.length));
+    const second = Math.floor((Math.random() * this.state.second.length));
+    const third = Math.floor((Math.random() * this.state.third.length));
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>{this.state.first[first]} {this.state.second[second]} {this.state.third[third]}</p>
       </div>
     );
   }
